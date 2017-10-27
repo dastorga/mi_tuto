d013d7379d56f3502a9f46dc9585c92ddd4f5e67
Dario <astorgadm@gmail.com>
2017-10-27 11:04:41 -0300

    admin.site.register(Document)

--- mi_app/models.py
+++ mi_app/models.py
@@ -17,7 +17,7 @@ class Alumno(models.Model):
 
     def __str__(self):
         return self.NombreCompleto()
-    
+
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
